92dd875632893222e93356bb1cbe650067162e2a
Gaby143 <gabzds@143@gmail.com>
2023-04-24 12:07:14 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,9 @@ int main(void)
 	float f;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
-Printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+Printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %nu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a float: %lu  byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
